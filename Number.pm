dcee6ebf3974195b675265deb5d9e49ae2426f0d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-04-07 11:58:48 +0200

    First version

---
+++ Number.pm
@@ -0,0 +1,73 @@
+package Mo::utils::Number;
+
+use base qw(Exporter);
+use strict;
+use warnings;
+
+use Error::Pure qw(err);
+use Readonly;
+#use Scalar::Util qw(blessed looks_like_number);
+
+Readonly::Array our @EXPORT_OK => qw(check_int check_natural
+	check_positive_natural);
+
+our $VERSION = 0.01;
+
+# .. -2, -1, 0, 1, 2, ...
+sub check_int {
+	my ($self, $key) = @_;
+
+	_check_key($self, $key) && return;
+
+	if ($self->{$key} !~ m/^\-?\d+$/ms) {
+		err "Parameter '$key' must be a integer.",
+			'Value', $self->{$key},
+		;
+	}
+
+	return;
+}
+
+# 0, 1, 2 ...
+sub check_natural {
+	my ($self, $key) = @_;
+
+	_check_key($self, $key) && return;
+
+	if ($self->{$key} !~ m/^\d+$/ms) {
+		err "Parameter '$key' must be a natural number.",
+			'Value', $self->{$key},
+		;
+	}
+
+	return;
+}
+
+# 1, 2 ...
+sub check_positive_natural {
+	my ($self, $key) = @_;
+
+	_check_key($self, $key) && return;
+
+	if ($self->{$key} !~ m/^\d+$/ms || $self->{$key} == 0) {
+		err "Parameter '$key' must be a positive natural number.",
+			'Value', $self->{$key},
+		;
+	}
+
+	return;
+}
+
+sub _check_key {
+	my ($self, $key) = @_;
+
+	if (! exists $self->{$key} || ! defined $self->{$key}) {
+		return 1;
+	}
+
+	return 0;
+}
+
+1;
+
+__END__
